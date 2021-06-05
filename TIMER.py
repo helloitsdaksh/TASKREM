8f184c492e3abc369e070ba169e023fbb6b51b5d
helloitsdaksh <helloitsdaksh007@gmail.com>
2021-06-05 23:01:33 +0530

    completed

--- TIMER.py
+++ TIMER.py
@@ -17,7 +17,6 @@ def currentTime():
         return time_Rem
 def sms(Text):
      
-<<<<<<< HEAD
     account_sid = "AC5e7263eb6fabe878ef609a3d6856da47"
     auth_token  = "3d5132767471e1036271987227d4e9f5"
     client = Client(account_sid, auth_token)
@@ -35,32 +34,16 @@ def email(Text):
 
     email.login("remtask@gmail.com", "Taskrem@2021") 
 
-    message = Text
+    message = "You A Have a Task to be reminded as of now!!!!!"
 
     email.sendmail("remtask@gmail.com", "hi5daksh@gmail.com", message) 
 
     email.quit()
-=======
-    account_sid = "YOUR SID"
-    auth_token  = "YOUR TOKEN"
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        to="Your number", 
-        from_="number you get from twilio",
-        body=Text)
-    print(message.sid)
-def email(Text):   
-    s = smtplib.SMTP('smtp.gmail.com', 587)     
-    s.starttls()    
-    s.login("sender's email id", "sender's password") 
-    message = Text
-    s.sendmail("sender's email id", "Reciver's email id", message) 
-    s.quit()
->>>>>>> 1435f79849208dca3cc8a75d285cfaa1da83b21e
+
 class Task_Rem:
     message = " "
     def task(self):
-        tasks = pd.read_csv('Task.csv')
+        tasks = pd.read_csv('/home/specter/Desktop/python/PROJECTS/TASKREM/Task.csv')
         sorted_Tasks = tasks.sort_values('time')
         X = sorted_Tasks.iloc[:,:-1].values
         y = sorted_Tasks.iloc[:,-1].values
@@ -71,10 +54,10 @@ class Task_Rem:
 
             if(y[0]== currentTime()):
                 engine.say(X[0][0])
-                sms(X[0][0])
+                # sms(X[0][0])
                 email(X[0][0])
                 engine.runAndWait() 
-                delete = pd.read_csv('Task.csv',index_col = "Task")
+                delete = pd.read_csv('/home/specter/Desktop/python/PROJECTS/TASKREM/Task.csv',index_col = "Task")
                 task= delete.drop(X[0][0])
                 df = pd.DataFrame(task,columns = ['time'])
                 print(df)
